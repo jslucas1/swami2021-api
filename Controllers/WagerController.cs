175364bdc1092bd37f0fcb0adb8e0ce6fff70777
Jeff Lucas <jslucas1@msn.com>
2021-07-28 07:29:12 -0500

    added wager query

--- Controllers/WagerController.cs
+++ Controllers/WagerController.cs
@@ -38,9 +38,28 @@ namespace swami2021.Controllers
         // GET: api/Wager/5
         [EnableCors("OpenPolicy")]
         [HttpGet("{id}", Name = "GetWager")]
-        public string Get(int id)
+        public List<ExpandoObject> Get(string id)
         {
-            return "value";
+            Console.WriteLine("made it to the get with " + id);
+            Database db = new Database();
+            db.Open();
+            string query = "select a.id, a.season, a.week, a.season_type, ";
+            query += "a.start_date, a.neutral_site, a.home_id, b.school as home_team, ";
+            query += "a.home_points, a.away_id, c.school as away_team, a.away_points, ";
+            query += "a.spread, a.formatted_spread, d.home_wager, d.away_wager, a.playable ";
+            query += "from game a ";
+            query += "LEFT JOIN team b on a.home_id = b.api_id ";
+            query += "LEFT JOIN team c on a.away_id = c.api_id ";
+            query += "LEFT JOIN wager d on a.api_id = d.game_api_id ";
+            query += "where a.week = 1 and d.google_id = @id";
+            var values = new Dictionary<string, object>()
+                {
+                    {"@id", id},
+                };
+            List<ExpandoObject> wagers = db.Select(query, values);
+            db.Close();
+
+            return wagers;
         }
 
         // POST: api/Wager
